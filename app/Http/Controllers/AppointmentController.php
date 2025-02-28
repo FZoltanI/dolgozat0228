303db146e534a6a57212c4a9ed037b757a5ba4b7
Zoltan Fabian <77056471+FZoltanI@users.noreply.github.com>
2025-02-28 10:32:57 +0100

    Update AppointmentController.php

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -8,12 +8,12 @@ use Illuminate\Validation\ValidationException;
 
 class AppointmentController extends Controller
 {
-    public function index(){
+    function index(){
         $appointments = Appointment::all()->load("barber");
         return response()->json($appointments, 200, [], JSON_UNESCAPED_UNICODE);
     }
 
-    public function store(Request $request){
+    function store(Request $request){
         try {
             $request->validate([
                 "name" => "required|string|max:255",
@@ -35,6 +35,27 @@ class AppointmentController extends Controller
             $appointment = Appointment::create($request->all());
             return response()->json(["success" => true, "message" => "Az időpont sikeresen rögzítve!"], 200, [], JSON_UNESCAPED_UNICODE);
         } catch (ValidationException $e) {
+            return response()->json(["success" => false, "message" => $e->errors()], 400, [], JSON_UNESCAPED_UNICODE);
+        }
+    }
+
+    function destroy(Request $request){
+        try{
+            $request->validate([
+                "id" => "required|integer|exists:appointments,id"
+            ], [
+                "required" => "A(z) :attribute megadása kötelező!",
+                "integer" => "A(z) :attribute csak szám lehet",
+                "exists" => "A megadott :attribute nem létezik."
+            ]);
+
+            $appointment = Appointment::findOrFail($request->id);
+            if ($appointment->delete()){
+                return response()->json(["success" => true, "message" => "Az időpont sikeresen lemondva!"], 200, [], JSON_UNESCAPED_UNICODE);
+            } else {
+                return response()->json(["success" => false, "message" => "Hiba az időpont lemondása közben!"], 404, [], JSON_UNESCAPED_UNICODE);
+            }
+        } catch(ValidationException $e){
             return response()->json(["success" => false, "message" => $e->errors()], 400, [], JSON_UNESCAPED_UNICODE);
         }
     }
