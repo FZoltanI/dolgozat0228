3e70cf23c93c4a79fb18bd6bc616a1cf5c526479
Zoltan Fabian <77056471+FZoltanI@users.noreply.github.com>
2025-02-28 10:03:41 +0100

    routes

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class BarberController extends Controller
 {
-    //
+    function index(){
+        $barbers = Barber::all();
+        return response()->json($barbers, 200, [], JSON_UNESCAPED_UNICODE);
+    }
+
+    function store(Request $request){
+    }
+
+    function destroy(Request $request){
+
+    }
 }
