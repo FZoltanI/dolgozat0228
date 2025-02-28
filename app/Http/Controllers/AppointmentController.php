32a02df9f846c9334349594c74052ef66f3ca0f2
Zoltan Fabian <77056471+FZoltanI@users.noreply.github.com>
2025-02-28 10:29:35 +0100

    Update AppointmentController.php

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Appointment;
+use Illuminate\Validation\ValidationException;
 
 class AppointmentController extends Controller
 {
-    //
+    public function index(){
+        $appointments = Appointment::all()->load("barber");
+        return response()->json($appointments, 200, [], JSON_UNESCAPED_UNICODE);
+    }
+
+    public function store(Request $request){
+        try {
+            $request->validate([
+                "name" => "required|string|max:255",
+                "barber_id" => "required|integer|exists:barbers,id",
+                "appointment" => "required|date",
+            ], [
+                "required" => "A(z) :attribute megadása kötelező!",
+                "string" => "A(z) :attribute csak szöveg lehet.",
+                "max" => "A(z) :attribute maximum :max karakter hosszú lehet.",
+                "integer" => "A(z) :attribute csak szám lehet",
+                "date" => "A(z) :attribute csak dátum lehet",
+                "exists" => "A megadott :attribute nem létezik."
+            ], [
+                "name" => "név",
+                "barber_id" => "barber azonosító",
+                "appointment" => "időpont"
+            ]);
+
+            $appointment = Appointment::create($request->all());
+            return response()->json(["success" => true, "message" => "Az időpont sikeresen rögzítve!"], 200, [], JSON_UNESCAPED_UNICODE);
+        } catch (ValidationException $e) {
+            return response()->json(["success" => false, "message" => $e->errors()], 400, [], JSON_UNESCAPED_UNICODE);
+        }
+    }
 }
